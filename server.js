e72d1782e5f0ef848860ec3b8e4d717d6407104a
AKmach <akeimach@users.noreply.github.com>
2017-11-27 21:31:05 -0800

    reformatting

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var db = require("./models");
 var routes = require("./controllers/burgers_controller.js");
 
 var app = express();
-// var PORT = process.env.PORT || 3000;
+
 app.use(express.static("public"));
 // app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
