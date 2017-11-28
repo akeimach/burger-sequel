26f4a61fac7e4252565f510186eb56af150fd9fb
AKmach <akeimach@users.noreply.github.com>
2017-11-27 22:16:59 -0800

    clean up

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
-var db = require("./models");
 var routes = require("./controllers/burgers_controller.js");
+var db = require("./models");
 
 var app = express();
-
 app.use(express.static("public"));
-// app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.text());
-// app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
-
-// app.use(bodyParser.urlencoded({ extended: false }));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
